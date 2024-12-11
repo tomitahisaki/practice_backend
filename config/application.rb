d2c3348559c61241200d1ec24177913cd8ce4f06
hisakey <alltimelow2011@gmail.com>
2024-12-11 09:22:25 +0900

    feat: annotate and generator setuped

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,19 @@ module BackpackerBackendPg
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: true,
+                       model_specs: true
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+      g.orm :active_record, migration: true, annotate: true
+    end
   end
 end
