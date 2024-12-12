99e827716b05735e38d09fea360bf09603b87bf7
hisakey <alltimelow2011@gmail.com>
2024-12-12 21:39:08 +0900

    feat: create job error logs model

---
+++ spec/models/job_error_log_spec.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: job_error_logs
+#
+#  id            :integer          not null, primary key
+#  detail        :string(255)      not null
+#  error_message :text(65535)      not null
+#  error_class   :string(255)      not null
+#  executed_at   :datetime         not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
+require 'rails_helper'
+
+RSpec.describe JobErrorLog, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
