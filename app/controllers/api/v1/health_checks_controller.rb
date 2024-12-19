f3f2753c53256cfc98a68645a5dd4f7976872a3a
hisakey <alltimelow2011@gmail.com>
2024-12-20 08:43:59 +0900

    fix: rubocop

--- app/controllers/api/v1/health_checks_controller.rb
+++ app/controllers/api/v1/health_checks_controller.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
-class Api::V1::HealthChecksController < ApplicationController
-  def index
-    render json: { message: 'API is running' }, status: :ok
+module Api
+  module V1
+    class HealthChecksController < ApplicationController
+      def index
+        render json: { message: 'API is running' }, status: :ok
+      end
+    end
   end
 end
