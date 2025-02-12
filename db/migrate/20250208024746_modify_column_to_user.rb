c6e6bc0d874f778c4bac05b7b5e645e35bd8364c
consolelogfuku <fuku1223sugawa@gmail.com>
2025-02-12 09:00:12 +0900

    fix: migrationの修正

--- db/migrate/20250208024746_modify_column_to_user.rb
+++ db/migrate/20250208024746_modify_column_to_user.rb
@@ -1,7 +1,11 @@
 class ModifyColumnToUser < ActiveRecord::Migration[8.0]
-  def change
+  def up
     change_column :users, :age, :integer
-
     add_index :users, :email, unique: true
+  end
+
+  def down
+    change_column :users, :age, :string
+    remove_index :users, :email
   end
 end
