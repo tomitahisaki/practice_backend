f1c2f43830ccf4e3dc5af135d9cc01e3f72e8c5a
consolelogfuku <fuku1223sugawa@gmail.com>
2025-02-16 00:37:36 +0900

    fix: specの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,7 +23,10 @@ RSpec.describe User, type: :model do
   let(:user) do
     build(
       :user,
-      first_name:
+      first_name:,
+      last_name:,
+      email:,
+      age:
     )
   end
 
@@ -31,6 +34,10 @@ RSpec.describe User, type: :model do
     subject { user.valid? }
 
     context 'first_name' do
+      let(:last_name) { 'last_name' }
+      let(:email) { 'example@example.com' }
+      let(:age) { 20 }
+
       context '10文字の時' do
         let(:first_name) { 'a' * 10 }
 
@@ -49,15 +56,129 @@ RSpec.describe User, type: :model do
       context 'nilの時' do
         let(:first_name) { nil }
 
-        it 'falseを返す' do
+        it 'falseを返し、エラーメッセージを返す' do
           expect(subject).to eq(false)
+          expect(user.errors.full_messages).to eq(['First nameを入力してください'])
         end
       end
       context '空文字の時' do
         let(:first_name) { '' }
 
+        it 'falseを返し、エラーメッセージを返す' do
+          expect(subject).to eq(false)
+          expect(user.errors.full_messages).to eq(['First nameを入力してください'])
+        end
+      end
+    end
+
+    context 'last_name' do
+      let(:first_name) { 'first_name' }
+      let(:email) { 'example@example.com' }
+      let(:age) { 20 }
+
+      context '10文字の時' do
+        let(:last_name) { 'a' * 10 }
+
+        it 'trueを返す' do
+          expect(subject).to eq(true)
+        end
+      end
+      context '11文字の時' do
+        let(:last_name) { 'a' * 11 }
+
+        it 'falseを返し、エラーメッセージを返す' do
+          expect(subject).to eq(false)
+          expect(user.errors.full_messages).to eq(['Last nameは10文字以内で入力してください'])
+        end
+      end
+      context 'nilの時' do
+        let(:last_name) { nil }
+
+        it 'falseを返し、エラーメッセージを返す' do
+          expect(subject).to eq(false)
+          expect(user.errors.full_messages).to eq(['Last nameを入力してください'])
+        end
+      end
+      context '空文字の時' do
+        let(:last_name) { '' }
+
+        it 'falseを返し、エラーメッセージを返す' do
+          expect(subject).to eq(false)
+          expect(user.errors.full_messages).to eq(['Last nameを入力してください'])
+        end
+      end
+    end
+
+    context 'email' do
+      let(:first_name) { 'first_name' }
+      let(:last_name) { 'first_name' }
+      let(:age) { 20 }
+
+      context '認められたformatではない時' do
+        let(:email) { 'example-example.com' }
+
+        it 'falseを返し、エラーメッセージを返す' do
+          expect(subject).to eq(false)
+          expect(user.errors.full_messages).to eq(['Emailは不正な値です'])
+        end
+      end
+      context 'nilの時' do
+        let(:email) { nil }
+
+        it 'falseを返し、エラーメッセージを返す' do
+          expect(subject).to eq(false)
+          expect(user.errors.full_messages).to eq(%w[Emailを入力してください Emailは不正な値です])
+        end
+      end
+      context '空文字の時' do
+        let(:email) { '' }
+
+        it 'falseを返し、エラーメッセージを返す' do
+          expect(subject).to eq(false)
+          expect(user.errors.full_messages).to eq(%w[Emailを入力してください Emailは不正な値です])
+        end
+      end
+    end
+
+    context 'age' do
+      let(:first_name) { 'Yamada' }
+      let(:last_name) { 'Taro' }
+      let(:email) { 'example@example.com' }
+
+      context '0の時' do
+        let(:age) { 0 }
+
+        it 'trueを返す' do
+          expect(subject).to eq(true)
+        end
+      end
+      context '-1の時' do
+        let(:age) { -1 }
+
         it 'falseを返す' do
           expect(subject).to eq(false)
+        end
+      end
+      context '文字列の時' do
+        let(:age) { 'a' }
+
+        it 'falseを返し、エラーメッセージを返す' do
+          expect(subject).to eq(false)
+          expect(user.errors.full_messages).to eq(['Ageは数値で入力してください'])
+        end
+      end
+      context 'nilの時' do
+        let(:age) { nil }
+
+        it 'trueを返す' do
+          expect(subject).to eq(true)
+        end
+      end
+      context '空文字の時' do
+        let(:age) { '' }
+
+        it 'trueを返す' do
+          expect(subject).to eq(true)
         end
       end
     end
@@ -65,8 +186,13 @@ RSpec.describe User, type: :model do
 
   describe '#full_name' do
     subject { user.full_name }
+
     context 'first_nameとlast_nameがある時' do
       let(:first_name) { 'Yamada' }
+      let(:last_name) { 'Taro' }
+      let(:email) { 'example@example.com' }
+      let(:age) { 20 }
+
       it '名前と苗字を結合して返す' do
         is_expected.to eq("#{user.first_name} #{user.last_name}")
       end
