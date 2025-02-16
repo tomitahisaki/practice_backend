92905593b2a458ba442d4a0685a288b3de288ffd
hisakey <alltimelow2011@gmail.com>
2025-02-16 16:24:34 +0900

    fix: authorize処理のコメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  before_action :authorized?
+  # before_action :authorized?
 
-  def authorized?
-    if request.headers['Authorization'] != 'Bearer 12346'
-      render json: { message: 'Unauthorized' }, status: :unauthorized
-    end
-  end
+  # def authorized?
+  #   if request.headers['Authorization'] != 'Bearer 12346'
+  #     render json: { message: 'Unauthorized' }, status: :unauthorized
+  #   end
+  # end
 end
