df8937a6ed71db88b2898efa1618adbfd153656b
hisakey <alltimelow2011@gmail.com>
2025-02-16 16:23:08 +0900

    feat: application_controllerにauthorize追加　

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :authorized?
+
+  def authorized?
+    if request.headers['Authorization'] != 'Bearer 12346'
+      render json: { message: 'Unauthorized' }, status: :unauthorized
+    end
+  end
 end
